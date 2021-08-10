ae5d04695257cca40d1933d347b127865c0d9f24
Martin Valba <kailing2@live.com>
2021-08-10 11:10:18 +0300

    Upstream some parts from termux-packages

--- scripts/properties.sh
+++ scripts/properties.sh
@@ -3,6 +3,8 @@ TERMUX_NDK_VERSION_NUM=21
 TERMUX_NDK_REVISION="d"
 TERMUX_NDK_VERSION=$TERMUX_NDK_VERSION_NUM$TERMUX_NDK_REVISION
 
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+
 if [ "${TERMUX_PACKAGES_OFFLINE-false}" = "true" ]; then
 	export ANDROID_HOME=${TERMUX_SCRIPTDIR}/build-tools/android-sdk
 	export NDK=${TERMUX_SCRIPTDIR}/build-tools/android-ndk
@@ -15,7 +17,7 @@ fi
 TERMUX_APP_PACKAGE="hilled.pwnterm"
 TERMUX_BASE_DIR="/data/data/${TERMUX_APP_PACKAGE}/files"
 TERMUX_CACHE_DIR="/data/data/${TERMUX_APP_PACKAGE}/cache"
-TERMUX_ANDROID_HOME="${TERMUX_BASE_DIR}/home"
+TERMUX_ANDROID_HOME="${TERMUX_BASE_DIR}/usr/home"
 TERMUX_PREFIX="${TERMUX_BASE_DIR}/usr"
 
 # Allow to override setup.
