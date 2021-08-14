beae7f83ce20a6153a2857bddca9a0e07b138707
Martin Valba <kailing2@live.com>
2021-08-14 09:35:08 +0300

    Update base

--- packages/base/build.sh
+++ packages/base/build.sh
@@ -1,8 +1,8 @@
 TERMUX_PKG_HOMEPAGE=https://gitlab.com/pwn-hunter/packages/base
 TERMUX_PKG_DESCRIPTION="Just for neofetch and other possible things "
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.0.5
-TERMUX_PKG_REVISION=3
+TERMUX_PKG_VERSION=1.0.6
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://gitlab.com/pwn-hunter/packages/base/-/archive/master/base-master.tar.gz
 TERMUX_PKG_SHA256=ec7acaac474e54afb538ca8829a220a02401283dca85383760304372a0bfa86b
 TERMUX_PKG_BUILD_IN_SRC=true
