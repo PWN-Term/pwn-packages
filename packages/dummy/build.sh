7149b8af07b2c1891fc574840da3c8d2dcb10b09
Martin Valba <kailing2@live.com>
2021-08-15 18:03:22 +0300

    [Build]: xfce4 deps for all arch types + tigervnc [1/3]

--- packages/dummy/build.sh
+++ packages/dummy/build.sh
@@ -6,8 +6,8 @@ TERMUX_PKG_VERSION=1.0.0
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/PWN-Term/dummy/archive/refs/heads/master.tar.gz
 TERMUX_PKG_SHA256=bf6bdabe1796cbafb747d84c683624a5000a6c8a0df97645911863483f00ae96
-# Edit me (any time)...
-TERMUX_PKG_DEPENDS="pcmanfm-qt, qterminal, lxqt-session, lxqt-panel, lxqt-runner, lxqt-openssh-askpass, lxqt-notificationd, lxqt-globalkeys, lxqt-config, lxqt-archiver, lxqt-about, lximage-qt, obconf-qt, lxqt-qtplugin, lxqt-themes, pavucontrol-qt"
+# xfce4-session, xfce4-settings, xfconf, xfwm4, tigervnc | [Build]: xfce4 deps for all arch types + tigervnc [1/3]
+TERMUX_PKG_DEPENDS="thunar, xfce4-panel"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 # ${TERMUX_PKG_DESCRIPTION} that is usable to build deps not like metapackage that makes it package but not dpe
