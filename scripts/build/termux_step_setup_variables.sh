cd57e736382da4fddae49d6bbee6fef1bf212e93
Martin Valba <kailing2@live.com>
2021-08-22 21:27:01 +0300

    Keep min sdk same with app
    Lets see how it goes...

--- scripts/build/termux_step_setup_variables.sh
+++ scripts/build/termux_step_setup_variables.sh
@@ -3,7 +3,7 @@ termux_step_setup_variables() {
 	: "${TERMUX_TOPDIR:="$HOME/.termux-build"}"
 	: "${TERMUX_ARCH:="aarch64"}" # arm, aarch64, i686 or x86_64.
 	: "${TERMUX_DEBUG:="false"}"
-	: "${TERMUX_PKG_API_LEVEL:="24"}"
+	: "${TERMUX_PKG_API_LEVEL:="28"}"
 	: "${TERMUX_NO_CLEAN:="false"}"
 	: "${TERMUX_QUIET_BUILD:="false"}"
 	: "${TERMUX_DEBDIR:="${TERMUX_SCRIPTDIR}/debs"}"
