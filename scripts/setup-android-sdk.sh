c67ec2aa6366e51bd56a4a756469ad54115fa926
Martin Valba <kailing2@live.com>
2021-08-23 14:51:43 +0300

    Changes related ndk and cmd-tools downloader

--- scripts/setup-android-sdk.sh
+++ scripts/setup-android-sdk.sh
@@ -5,9 +5,7 @@ set -e -u
 . $(cd "$(dirname "$0")"; pwd)/properties.sh
 
 ANDROID_SDK_FILE=commandlinetools-linux-7583922_latest.zip
-ANDROID_SDK_SHA256=124f2d5115eee365df6cf3228ffbca6fc3911d16f8025bebd5b1c6e2fcfa7faf
 ANDROID_NDK_FILE=android-ndk-r${TERMUX_NDK_VERSION}-linux.zip
-ANDROID_NDK_SHA256=e3eacf80016b91d4cd2c8ca9f34eebd32df912bb799c859cc5450b6b19277b4f
 if [ ! -d $ANDROID_HOME ]; then
 	mkdir -p $ANDROID_HOME
 	cd $ANDROID_HOME/..
@@ -19,7 +17,6 @@ if [ ! -d $ANDROID_HOME ]; then
 	curl --fail --retry 3 \
 		-o tools.zip \
 		https://dl.google.com/android/repository/${ANDROID_SDK_FILE}
-	echo "${ANDROID_SDK_SHA256} tools.zip" | sha256sum -c -
 	rm -Rf android-sdk
 	unzip -q tools.zip -d android-sdk
 	rm tools.zip
@@ -32,7 +29,6 @@ if [ ! -d $NDK ]; then
 	echo "Downloading android ndk..."
 	curl --fail --retry 3 -o ndk.zip \
 		https://dl.google.com/android/repository/${ANDROID_NDK_FILE}
-	echo "${ANDROID_NDK_SHA256} ndk.zip" | sha256sum -c -
 	rm -Rf android-ndk-r$TERMUX_NDK_VERSION
 	unzip -q ndk.zip
 	mv android-ndk-r$TERMUX_NDK_VERSION $(basename $NDK)
