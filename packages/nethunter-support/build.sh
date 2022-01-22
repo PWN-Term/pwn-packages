025227c5753c6dc9008ace8e1fa236f73933597b
Martin Valba <kailing2@live.com>
2022-01-22 17:16:19 +0200

    Fixup sha256sum

--- packages/nethunter-support/build.sh
+++ packages/nethunter-support/build.sh
@@ -7,7 +7,7 @@ TERMUX_PKG_VERSION=1.0.4
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_MAINTAINER=@Martinvlba
 TERMUX_PKG_SRCURL=https://github.com/PWN-Term/nethunter-support/archive/refs/heads/master.tar.gz
-TERMUX_PKG_SHA256=47904d566d6f54c47e2ae8e63a5c2b1f4e7746dd0e9185c9bbd4a1fc922f053d
+TERMUX_PKG_SHA256=cb39bd868dc7c87435dee818855a3677bf47a4ffc32dc5bbb0efc5d884b3f779
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_post_make_install() {
