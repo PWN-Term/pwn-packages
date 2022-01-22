5f7cd0855f77bd76b0ed6d75a3a3b8d3a6e5808e
Martin Valba <kailing2@live.com>
2022-01-22 17:00:39 +0200

    Force grep to be built

--- packages/grep/build.sh
+++ packages/grep/build.sh
@@ -1,3 +1,5 @@
+# Forced grep build 1
+
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/grep/
 TERMUX_PKG_DESCRIPTION="Command which searches one or more input files for lines containing a match to a specified pattern"
 TERMUX_PKG_LICENSE="GPL-3.0"
