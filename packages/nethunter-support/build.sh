876b8f2c36c571027749b7d805abdaa019302fce
Martin Valba <kailing2@live.com>
2022-01-23 14:49:37 +0200

    Will error out ( Nethunter-support )

--- packages/nethunter-support/build.sh
+++ packages/nethunter-support/build.sh
@@ -3,7 +3,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/PWN-Term/nethunter-support
 TERMUX_PKG_DESCRIPTION="Adds support for nh when terminal gets called via modified nethunter-app"
 TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_VERSION=1.0.4
+TERMUX_PKG_VERSION=1.0.5
 TERMUX_PKG_REVISION=1
 TERMUX_PKG_MAINTAINER=@Martinvlba
 TERMUX_PKG_SRCURL=https://github.com/PWN-Term/nethunter-support/archive/refs/heads/master.tar.gz
