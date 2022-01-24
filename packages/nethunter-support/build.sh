d49bcb564c00237e5d3852fed0104f6153f3bd70
Martin Valba <kailing2@live.com>
2022-01-24 19:29:12 +0200

    Build updated nethunter-support

--- packages/nethunter-support/build.sh
+++ packages/nethunter-support/build.sh
@@ -4,10 +4,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/PWN-Term/nethunter-support
 TERMUX_PKG_DESCRIPTION="Adds support for nh when terminal gets called via modified nethunter-app"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=1.0.5
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_REVISION=3
 TERMUX_PKG_MAINTAINER=@Martinvlba
 TERMUX_PKG_SRCURL=https://github.com/PWN-Term/nethunter-support/archive/refs/heads/master.tar.gz
-TERMUX_PKG_SHA256=5771c7383663193d01861c85e13fc081019a52c3d9049179d50affdca73553e2
+TERMUX_PKG_SHA256=4d769b9c200871e22798a0947631f88f706426878fbf592a9a92698e81a5faac
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_post_make_install() {
