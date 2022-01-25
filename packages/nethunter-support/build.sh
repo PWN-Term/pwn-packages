9133f453b544846521ae7bf8ed0d46936b61fab5
Martin Valba <kailing2@live.com>
2022-01-25 16:12:17 +0200

    Force nethunter-support to be built again

--- packages/nethunter-support/build.sh
+++ packages/nethunter-support/build.sh
@@ -7,7 +7,7 @@ TERMUX_PKG_VERSION=1.0.5
 TERMUX_PKG_REVISION=4
 TERMUX_PKG_MAINTAINER=@Martinvlba
 TERMUX_PKG_SRCURL=https://github.com/PWN-Term/nethunter-support/archive/refs/heads/master.tar.gz
-TERMUX_PKG_SHA256=2aadf6a667bf0d64cdd187e589851b2f943ea65ae91c9615be7c04364d81795d
+TERMUX_PKG_SHA256=be26473e69aaf4bd813aaf49279376d76e1f1ea9634ae20c6ddafae10d35ad98
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_post_make_install() {
