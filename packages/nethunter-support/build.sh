ba43517ae1f3fee799852d2ab1e8c010358853f3
Martin Valba <kailing2@live.com>
2022-01-27 17:56:56 +0200

    Rebuild nethunter-support

--- packages/nethunter-support/build.sh
+++ packages/nethunter-support/build.sh
@@ -7,7 +7,7 @@ TERMUX_PKG_VERSION=1.0.5
 TERMUX_PKG_REVISION=5
 TERMUX_PKG_MAINTAINER=@Martinvlba
 TERMUX_PKG_SRCURL=https://github.com/PWN-Term/nethunter-support/archive/refs/heads/master.tar.gz
-TERMUX_PKG_SHA256=a0c7bf8290f79aca30abb6cdc5ce45ca8fea51d7985401e1b402710f57310183
+TERMUX_PKG_SHA256=1a03887c79cbad6f9912a2926c7fba2747f5144ddc756d32230b846bf397a835
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_post_make_install() {
