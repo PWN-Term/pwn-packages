4d9ecb8caae9b5dd2cdb70053ca70d4a24fcfffa
Martin Valba <kailing2@live.com>
2022-06-01 11:57:07 +0300

    Update link for base
    Also re-compile/install it with new changes

--- packages/base/build.sh
+++ packages/base/build.sh
@@ -6,8 +6,9 @@ TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER=@hilledkinged
 TERMUX_PKG_VERSION=1.0.7
 TERMUX_PKG_REVISION=1
-TERMUX_PKG_SRCURL=https://gitlab.com/pwn-hunter/packages/base/-/archive/master/base-master.tar.gz
-TERMUX_PKG_SHA256=6d056e95fd741400ff04a924fd00a70c583b56c3497ed9800616f9c6ac5ba75e
+COMMIT_ID=126bf7ba2c1966ad8e766eb5a9b4793a5e10d19a
+TERMUX_PKG_SRCURL=https://gitlab.com/pwn-hunter/packages/base/-/archive/$COMMIT_ID/base-$COMMIT_ID.tar.gz
+TERMUX_PKG_SHA256=e110146c986d504716b391d5fa108784d4b556f5a39cf29a3ded3c35c640ccf3
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_post_make_install() {
