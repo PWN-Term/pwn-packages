dc5595c9826afb90fdd220dc6f7a3c8efd66c1b2
root <root@localhost>
2022-10-16 13:15:28 +0000

    Rebuild our own modified jdk17/openjdk-17

--- packages/openjdk-17/build.sh
+++ packages/openjdk-17/build.sh
@@ -3,14 +3,17 @@ TERMUX_PKG_DESCRIPTION="Java development kit and runtime"
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=17.0
-TERMUX_PKG_REVISION=17
-TERMUX_PKG_SRCURL=https://github.com/termux/openjdk-mobile-termux/archive/ec285598849a27f681ea6269342cf03cf382eb56.tar.gz
-TERMUX_PKG_SHA256=d7c6ead9d80d0f60d98d0414e9dc87f5e18a304e420f5cd21f1aa3210c1a1528
-TERMUX_PKG_DEPENDS="freetype, giflib, libandroid-shmem, libandroid-spawn, libiconv, zlib, xorgproto, libx11, libxcursor, libxext, cups, fontconfig, libjpeg-turbo, libpng, libxrender, libxtst, libxrandr, libxt, libxi"
+TERMUX_PKG_REVISION=11
+TERMUX_PKG_SRCURL=https://github.com/PWN-Term/mobile/archive/7130ec5e646cddc77af2e3adf2b0869757f50619.tar.gz
+TERMUX_PKG_SHA256=7ebe65d11ffbe8fc09c6b179a85b42eca13c8f7fad0eee50a89e41df1b908be4
+TERMUX_PKG_DEPENDS="freetype, libandroid-shmem, libandroid-spawn, libiconv, zlib, xorgproto, libx11, libxcursor, libxext, cups, fontconfig, libpng, libxrender, libxtst, libxrandr, libxt, libxi"
 TERMUX_PKG_BUILD_DEPENDS="cups, fontconfig, libpng, libx11, libxrender"
 TERMUX_PKG_SUGGESTS="cups, fontconfig, libx11, libxrender"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_HAS_DEBUG=false
+TERMUX_PKG_NO_ELF_CLEANER=true
+
+# Force workflow
 
 termux_step_pre_configure() {
 	unset JAVA_HOME
@@ -73,18 +76,11 @@ termux_step_configure() {
 		--with-fontconfig-include="$TERMUX_PREFIX/include" \
 		--with-freetype-include="$TERMUX_PREFIX/include/freetype2" \
 		--with-freetype-lib="$TERMUX_PREFIX/lib" \
-		--with-giflib=system \
-		--with-libjpeg=system \
 		--with-libpng=system \
 		--with-zlib=system \
 		--x-includes="$TERMUX_PREFIX/include/X11" \
 		--x-libraries="$TERMUX_PREFIX/lib" \
-		--with-x="$TERMUX_PREFIX/include/X11" \
-		AR="$AR" \
-		NM="$NM" \
-		OBJCOPY="$OBJCOPY" \
-		OBJDUMP="$OBJDUMP" \
-		STRIP="$STRIP"
+        	--with-x="$TERMUX_PREFIX/include/X11"
 }
 
 termux_step_make() {
@@ -108,17 +104,7 @@ termux_step_make_install() {
 		if [ ! -f "$i" ]; then
 			continue
 		fi
-		ln -sfr "$i" "$TERMUX_PREFIX/bin/$(basename "$i")"
-	done
-
-	# Link manpages to location accessible by "man".
-	mkdir -p $TERMUX_PREFIX/share/man/man1
-	for i in $TERMUX_PREFIX/opt/openjdk/man/man1/*; do
-		if [ ! -f "$i" ]; then
-			continue
-		fi
-		gzip "$i"
-		ln -sfr "${i}.gz" "$TERMUX_PREFIX/share/man/man1/$(basename "$i").gz"
+		ln -sfr $i $TERMUX_PREFIX/bin/$(basename $i)
 	done
 
 	# Dependent projects may need JAVA_HOME.
@@ -128,11 +114,7 @@ termux_step_make_install() {
 
 	# Symlink external dependencies.
 	local l
-	for l in libandroid-shmem.so libandroid-spawn.so libfreetype.so \
-		libiconv.so libz.so.1 libXext.so libX11.so libXrender.so \
-		libXrender.so.1 libXrender.so.1.3.0 libXtst.so libXtst.so.6 \
-		libXtst.so.6.1.0 libXi.so libxcb.so libXau.so libXdmcp.so \
-		libfreetype.so libfontconfig.so; do
+	for l in libandroid-shmem.so libandroid-spawn.so libfreetype.so libiconv.so libz.so.1; do
 		ln -sfr $TERMUX_PREFIX/lib/$l \
 			$TERMUX_PREFIX/opt/openjdk/lib/$l
 	done
