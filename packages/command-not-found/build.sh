e73dc150295aeb63656b3d0c6ea53402ad86df6e
martinvlba <kailing2@live.com>
2022-10-16 13:29:53 +0000

    Make sure to compile our command-not-found

--- packages/command-not-found/build.sh
+++ packages/command-not-found/build.sh
@@ -1,7 +1,7 @@
-TERMUX_PKG_HOMEPAGE=https://github.com/termux/command-not-found
+TERMUX_PKG_HOMEPAGE=https://github.com/PWN-Term/command-not-found
 TERMUX_PKG_DESCRIPTION="Suggest installation of packages in interactive shell sessions"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1.72
-TERMUX_PKG_SRCURL=https://github.com/termux/command-not-found/archive/${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=3da2176050cc6ba832be1539103ae74dad0280b8d9517e8149b2fc9210f8682c
+TERMUX_PKG_VERSION=1.63
+TERMUX_PKG_SRCURL=https://github.com/PWN-Term/command-not-found/archive/master.tar.gz
+TERMUX_PKG_SHA256=aafed1fe7a603d104a1e51c2683f3d5af221833b78e5bf9c6508a0ddddafe858
